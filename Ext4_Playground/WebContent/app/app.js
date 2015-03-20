9ae4b9279bd5eb926022b020fd81d6c08248ddf1
PramodKhare <kharepr@ccs.neu.edu>
2015-03-20 02:00:37 -0400

    Commtting Initial Codes - Testing Ext4 with multiple layouts
    Experimenting with ExtJS4

---
+++ Ext4_Playground/WebContent/app/app.js
@@ -0,0 +1,13 @@
+/**
+ * Author : Pramod
+ * 
+ */
+
+    Ext.application({
+      name : 'Application',
+      // Automatically create an instance of AM.view.Viewport
+      // on application launch
+      autoCreateViewport : true,
+      // Attach controllers
+      controllers : ['ItemController']
+    });
